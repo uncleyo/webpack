d4a7aac61c5cd43d9e80782ffaab1044e546536d
Yuriy Ostapenko <yuo@scalepoint.com>
2017-01-04 21:50:52 +0100

    Test prototype (doesn't work) for a multicompiler shared watch trigger

--- test/watchCases/simple/multi-compiler/0/index.js
+++ test/watchCases/simple/multi-compiler/0/index.js
@@ -1,11 +1,15 @@
 require("./changing-file")
+require("./shared-changing-file")
 it("should watch for changes", function() {
 	switch(WATCH_STEP) {
 		case "0":
-			STATS_JSON.children.should.have.size(2);
+			STATS_JSON.children.should.have.size(3);
 			break;
 		case "1":
 			STATS_JSON.children.should.have.size(1);
+			break;
+		case "2":
+			STATS_JSON.children.should.have.size(2);
 			break;
 	}
 })
--- test/watchCases/simple/multi-compiler/webpack.config.js
+++ test/watchCases/simple/multi-compiler/webpack.config.js
@@ -5,9 +5,15 @@ module.exports = [{
 		filename: "./bundle.js"
 	}
 }, {
+	name: "changing2",
+	entry: "./shared-changing-file.js",
+	output: {
+		filename: "./bundle-2.js"
+	}
+}, {
 	name: "static",
 	entry: "./static-file.js",
 	output: {
-		filename: "./static.js"
+	filename: "./static.js"
 	}
 }];
